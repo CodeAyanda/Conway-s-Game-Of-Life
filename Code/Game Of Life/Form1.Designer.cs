4c651a1769221d4e0f5ef1d675ac88a44f130aa5
Ayanda Gumede <ayandagumede1999@gmail.com>
2023-01-06 21:42:20 +0200

    Add files via upload

---
+++ Code/Game Of Life/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Game_Of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(1007, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Population";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(1007, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Starting Population";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(1007, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Frame Count ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1438, 953);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Code/Game Of Life/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Of_Life
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int[,] grid = new int[numOfCells, numOfCells];
+        int[,] next = new int[numOfCells, numOfCells];
+        static int numOfCells = 200;
+        int size = 4;
+
+        Pen white = new Pen(Color.White);
+        SolidBrush whiteFill = new SolidBrush(Color.White);
+        public Form1()
+        {
+            int count = 0;
+            InitializeComponent();
+            for (int i = 0; i < numOfCells; i++)
+            {
+                for (int j = 0; j < numOfCells; j++)
+                {
+                    grid[i, j] = rnd.Next(0, 2);
+                    if (grid[i, j] ==1)
+                    {
+                        count++;
+                    }
+                }
+
+            }
+            label2.Text = "Starting Population : " + count.ToString();
+
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            InitializeNext();
+            DrawGrid(g, next);
+
+            label1.Text = "Current Population : " + CountPop(grid).ToString();
+            label3.Text = "Frame Count : " + frameCount.ToString();
+        }
+
+        public void DrawGrid(Graphics g, int[,] array)
+        {
+            for (int i = 0; i < numOfCells; i++)
+            {
+                for (int j = 0; j < numOfCells; j++)
+                {
+                    if (array[i, j] == 1)
+                    {
+                        g.FillRectangle(whiteFill, i * size, j * size, size - 1, size - 1);
+                    }
+
+                }
+            }
+
+
+        }
+
+        public int CountPop(int[,] array)
+        {
+            int count = 0;
+            for (int i = 0; i < numOfCells; i++)
+            {
+                for (int j = 0; j < numOfCells; j++)
+                {
+                    if(array[i, j] == 1)
+                    {
+                        count++;
+                    }
+
+                }
+
+            }
+            return count;
+
+
+        }
+
+        public void InitializeNext()
+        {
+            //Compute next gen
+            for (int i = 0; i < numOfCells; i++)
+            {
+                for (int j = 0; j < numOfCells; j++)
+                {
+                    if (grid[i, j] == 0 && Neighbours(i, j) == 3)
+                    {
+                        next[i, j] = 1;
+                    }
+                    else if (grid[i, j] == 1 && (Neighbours(i, j) == 2 || Neighbours(i, j) == 3))
+                    {
+                        next[i, j] = 1;
+                    }
+                    else
+                    {
+                        next[i, j] = 0;
+                    }
+
+                }
+
+            }
+            //grid == next
+            for (int i = 0; i < numOfCells; i++)
+            {
+                for (int j = 0; j < numOfCells; j++)
+                {
+                    grid[i, j] = next[i, j];
+                }
+            }
+
+
+        }
+
+        public int Neighbours(int a, int b)
+        {
+            int sum = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if(a == 0 || b == 0  || b == numOfCells-1 || a == numOfCells-1 || (i == 0 && j == 0))
+                    {
+                    }
+                    else
+                    {
+                        sum = sum + grid[a + i, b + j];
+
+                    }
+                }
+
+            }
+
+            return sum;
+
+
+        }
+
+        int frameCount = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+            frameCount++;
+        }
+    }
+}
